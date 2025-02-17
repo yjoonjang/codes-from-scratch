4fd2c0a7c5b88925c250b80ead37c1fbba991c2c
torchtorchkimtorch <mhkim092929@gmail.com>
2025-02-17 19:06:51 +0900

    add transformer.py template

---
+++ 1_transformers/transformer.py
@@ -0,0 +1,11 @@
+
+
+class Transformer(Module):
+
+class TransformerEncoder(Module):
+
+class TransformerDecoder(Module):
+
+class TransformerEncoderLayer(Module):
+
+class TransformerDecoderLayer(Module):
